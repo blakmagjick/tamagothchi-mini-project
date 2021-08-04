b53bd4444cefae4ca5c404561f7277ac17444274
Suzy Q <suzy@iamsuzyq.com>
2021-08-03 20:37:06 -0700

    Cleaned up a lot of js, after setting up Class and Object. Game starts, but buttons aren't working.

--- js/game.js
+++ js/game.js
@@ -6,22 +6,13 @@ class Tamagothchi {
         this.boredom = 1;
         this.age = 0;
         this.alive = true;
-        this.time = 0
+        this.agingTime = 0
     }
-    // iHunger(){
-    //     this.hunger++
-    // },
-    // iAmTired(){
-    //     this.sleepness++
-    // },
-    // iAmBored(){
-    //     this.boredom++
-    // }
     movingAround(){
         const movements = ['flex-start', 'flex-end', 'center']
         let randoMovement1 = Math.floor(Math.random() * movements.length)
         let randoMovement2 = Math.floor(Math.random() * movements.length)
-        if (this.time % 2 === 0) {
+        if (this.agingTime % 2000 === 0) {
         let leftRight = document.querySelector('#skelly-container')
         let upDown = document.querySelector('#skelly-container')
         leftRight.style.justifyContent = movements[randoMovement1]
@@ -29,7 +20,9 @@ class Tamagothchi {
         }
     }
     closerToDeath(){
-        this.age++
+        if (this.agingTime % 5000 === 0){
+            this.age++
+        }
     }
     glowUp(){
         if (this.age >= 10){
@@ -37,80 +30,73 @@ class Tamagothchi {
             skellySprite.style.height = '100px'
         }
     }
+    deathBecomesHer() {
+        if (this.hunger >= 10) {
+            alert('Oh bones, you starved to death!')
+            // console.log('Oh bones, you starved to death!')
+        } else if (this.sleepness >= 10) {
+            alert('Oh bones, you were so tired you died!')
+            // console.log('Oh bones, you were so tired you died!')
+        } else if (this.boredom >= 10) {
+            alert('Oh bones, you died of boredom!')
+            // console.log('Oh bones, you died of boredom!')
+        }
+    }
 }
 
 const gamePlay = {
-    tamagothchi: null;
+    tamagothchi: null,
+    runningTime: null,
 
-}
-
-const pet = new Tamagothchi('Stevie')
-console.log(pet)
-
-// Global variables set for easy use in below functions
-let hungerScore = document.querySelector('#hungerNum')
-let tiredScore = document.querySelector('#sleepyNum')
-let boredScore = document.querySelector('#boredNum')
-
-// Feed Me Button //
-feedMePlz = () => {
-    pet.hunger--
-    hungerScore.innerHTML = pet.hunger 
-    console.log('I\'m hungry yo!')
-}
-const feedMe = document.querySelector('#feedme')
-feedMe.addEventListener('click', feedMePlz)
-
-// Sleepy Time Button //
-sleepNowPlz = () => {
-    pet.sleepness--
-    tiredScore.innerHTML = pet.sleepness
-    console.log('*yawns*')
-}
-const sleepNow = document.querySelector('#sleepnow')
-sleepNow.addEventListener('click', sleepNowPlz)
-
-// Let's Play Button //
-playWithMePlz = () => {
-    pet.boredom--
-    boredScore.innerHTML = pet.boredom
-    console.log('I\'m so bored!')
-}
-const playThing = document.querySelector('#plaything')
-playThing.addEventListener('click', playWithMePlz)
-
-// But Did You Die? //
-deathBecomesHer = () => {
-    if (pet.hunger >= 10) {
-        // alert('Oh bones, you starved to death!')
-        console.log('Oh bones, you starved to death!')
-    } else if (pet.sleepness >= 10) {
-        // alert('Oh bones, you were so tired you died!')
-        console.log('Oh bones, you were so tired you died!')
-    } else if (pet.boredom >= 10) {
-        // alert('Oh bones, you died of boredom!')
-        console.log('Oh bones, you died of boredom!')
+    startAtTheBeginning(){
+        const nameInput = document.getElementById('namevalue').value
+        const petName = document.getElementById('hellothere')
+        petName.innerText = `Let's play ${nameInput}!`
+        const newPet = new Tamagothchi(nameInput);
+        this.tamagothchi = newPet
+        this.startGame()
+    },
+    startGame() {
+        let hungerScore = document.querySelector('#hungerNum')
+        let tiredScore = document.querySelector('#sleepyNum')
+        let boredScore = document.querySelector('#boredNum')
+        setInterval(() => {
+        this.runningTime++
+        this.tamagothchi.hunger++
+        hungerScore.innerHTML = this.tamagothchi.hunger
+        this.tamagothchi.sleepness++
+        tiredScore.innerHTML = this.tamagothchi.sleepness
+        this.tamagothchi.boredom++
+        boredScore.innerHTML = this.tamagothchi.boredom
+    }, 1000)
+    },
+    feedMePlz() {
+        // let hungerScore1 = document.querySelector('#hungerNum')
+        // this.tamagothchi.hunger--
+        // hungerScore1.innerHTML = this.tamagothchi.hunger 
+        console.log('I\'m hungry yo!')
+    },
+    sleepNowPlz() {
+        // let tiredScore1 = document.querySelector('#sleepyNum')
+        // this.tamagothchi.sleepness--
+        // tiredScore1.innerHTML = this.tamagothchi.sleepness
+        console.log('*yawns*')
+    },
+    playWithMePlz() {
+        // let boredScore1 = document.querySelector('#boredNum')
+        // this.tamagothchi.boredom--
+        // boredScore1.innerHTML = this.tamagothchi.boredom
+        console.log('I\'m so bored!')
     }
 }
-deathBecomesHer()
+// deathBecomesHer()
 
-// Timer To Rule Them All //
-startGame = () => {
-    // let skellySprite = document.querySelector('#skelly')
-    // console.log(skellySprite)
-    //         // skellySprite.style.height = '85px'
-    const nameInput = document.getElementById('namevalue').value
-    const name = document.getElementById('hellothere')
-    name.innerText = `Let's play ${nameInput}!`
-    console.log(name)
-    setInterval(() => {
-    pet.time++
-    pet.hunger++
-    hungerScore.innerHTML = pet.hunger
-    pet.sleepness++
-    tiredScore.innerHTML = pet.sleepness
-    pet.boredom++
-    boredScore.innerHTML = pet.boredom
-}, 1000)
-}
-// startGame()
+// ButtonListeners
+const feedMe = document.querySelector('#feedme')
+feedMe.addEventListener('click', gamePlay.feedMePlz)
+
+const sleepNow = document.querySelector('#sleepnow')
+sleepNow.addEventListener('click', gamePlay.sleepNowPlz)
+
+const playThing = document.querySelector('#plaything')
+playThing.addEventListener('click', gamePlay.playWithMePlz)
