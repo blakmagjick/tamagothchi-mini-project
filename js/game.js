da8db6049105a89847dd9b55789ea17d0fd69a7e
Suzy Q <suzy@iamsuzyq.com>
2021-08-03 17:27:33 -0700

    Got the startGame timer to work

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ class Tamagothchi {
         this.boredom = 1;
         this.age = 0;
         this.alive = true;
-        // this.time = 0
+        this.time = 0
     }
     getHungry() {
         this.hunger++
@@ -22,13 +22,10 @@ class Tamagothchi {
 const pet = new Tamagothchi('Stevie')
 console.log(pet)
 
-// Setting up game to have pet's stats, and make it for easy changing in functions below. 
+// Global variables set for easy use in below functions
 let hungerScore = document.querySelector('#hungerNum')
-// hungerScore.innerHTML = pet.hunger
 let tiredScore = document.querySelector('#sleepyNum')
-tiredScore.innerHTML = pet.sleepness
 let boredScore = document.querySelector('#boredNum')
-boredScore.innerHTML = pet.boredom
 
 // Moving Skeleton
 const movements = ['flex-start', 'flex-end', 'center']
@@ -69,3 +66,27 @@ playWithMePlz = () => {
 const playThing = document.querySelector('#plaything')
 playThing.addEventListener('click', playWithMePlz)
 
+// But Did You Die? //
+deathBecomesHer = () => {
+    if (hungerScore >= 10 || tiredScore >= 10 || boredScore >= 10) {
+        // Add code to change image to dead skelly
+        // Change this to have seperate checks so you can write different console logs. 
+        console.log ('Oh bones...you died!')
+    }
+}
+
+// Timer To Rule Them All //
+startGame = () => {
+    setInterval(() => {
+    pet.time+=2
+    pet.hunger++
+    hungerScore.innerHTML = pet.hunger
+    pet.sleepness++
+    tiredScore.innerHTML = pet.sleepness
+    pet.boredom++
+    boredScore.innerHTML = pet.boredom
+    
+}, 1000)
+}
+
+startGame()
