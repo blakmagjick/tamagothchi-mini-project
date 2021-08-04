7e5707accced2926cefda8f95f3a54574e7da098
Suzy Q <suzy@iamsuzyq.com>
2021-08-03 19:44:22 -0700

    Started refactoring code into a Class and game Object

--- js/game.js
+++ js/game.js
@@ -8,6 +8,40 @@ class Tamagothchi {
         this.alive = true;
         this.time = 0
     }
+    // iHunger(){
+    //     this.hunger++
+    // },
+    // iAmTired(){
+    //     this.sleepness++
+    // },
+    // iAmBored(){
+    //     this.boredom++
+    // }
+    movingAround(){
+        const movements = ['flex-start', 'flex-end', 'center']
+        let randoMovement1 = Math.floor(Math.random() * movements.length)
+        let randoMovement2 = Math.floor(Math.random() * movements.length)
+        if (this.time % 2 === 0) {
+        let leftRight = document.querySelector('#skelly-container')
+        let upDown = document.querySelector('#skelly-container')
+        leftRight.style.justifyContent = movements[randoMovement1]
+        upDown.style.alignItems = movements[randoMovement2]
+        }
+    }
+    closerToDeath(){
+        this.age++
+    }
+    glowUp(){
+        if (this.age >= 10){
+            let skellySprite = document.querySelector('#skelly')
+            skellySprite.style.height = '100px'
+        }
+    }
+}
+
+const gamePlay = {
+    tamagothchi: null;
+
 }
 
 const pet = new Tamagothchi('Stevie')
@@ -17,18 +51,6 @@ console.log(pet)
 let hungerScore = document.querySelector('#hungerNum')
 let tiredScore = document.querySelector('#sleepyNum')
 let boredScore = document.querySelector('#boredNum')
-
-// Moving Skeleton
-const movements = ['flex-start', 'flex-end', 'center']
-movingAround = () => {
-    let randoMovement1 = Math.floor(Math.random() * movements.length)
-    let randoMovement2 = Math.floor(Math.random() * movements.length)
-    let leftRight = document.querySelector('#skelly-container')
-    let upDown = document.querySelector('#skelly-container')
-    leftRight.style.justifyContent = movements[randoMovement1]
-    upDown.style.alignItems = movements[randoMovement2]
-}
-// movingAround()
 
 // Feed Me Button //
 feedMePlz = () => {
@@ -59,21 +81,30 @@ playThing.addEventListener('click', playWithMePlz)
 
 // But Did You Die? //
 deathBecomesHer = () => {
-    if (hungerScore >= 10 || tiredScore >= 10 || boredScore >= 10) {
-        // Add code to change image to dead skelly
-        // Change this to have seperate checks so you can write different console logs. 
-        console.log ('Oh bones...you died!')
+    if (pet.hunger >= 10) {
+        // alert('Oh bones, you starved to death!')
+        console.log('Oh bones, you starved to death!')
+    } else if (pet.sleepness >= 10) {
+        // alert('Oh bones, you were so tired you died!')
+        console.log('Oh bones, you were so tired you died!')
+    } else if (pet.boredom >= 10) {
+        // alert('Oh bones, you died of boredom!')
+        console.log('Oh bones, you died of boredom!')
     }
 }
+deathBecomesHer()
 
 // Timer To Rule Them All //
 startGame = () => {
+    // let skellySprite = document.querySelector('#skelly')
+    // console.log(skellySprite)
+    //         // skellySprite.style.height = '85px'
     const nameInput = document.getElementById('namevalue').value
     const name = document.getElementById('hellothere')
     name.innerText = `Let's play ${nameInput}!`
     console.log(name)
     setInterval(() => {
-    pet.time+=2
+    pet.time++
     pet.hunger++
     hungerScore.innerHTML = pet.hunger
     pet.sleepness++
